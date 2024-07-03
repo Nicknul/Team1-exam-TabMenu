24ee49e333410928ec3c8e815337a867d391e3f0
KimInteger <kjs20151240@gmail.com>
2024-07-03 15:45:30 +0900

    feat_김정수 : nameArr.forEach를 사용하여 ul태그에 li append

--- src/tabMenu_JS.ts
+++ src/tabMenu_JS.ts
@@ -1,1 +1,12 @@
-console.log('hellow tab');
+const root : HTMLElement = document.getElementById('root') as HTMLDivElement;
+
+const ul : HTMLElement = document.createElement('ul') as HTMLUListElement;
+
+const nameArr : string[] = ["구하림","김정수","송이현","이연승","정호연"];
+
+nameArr.forEach((item : string) : void =>{
+  const li : HTMLLIElement = document.createElement('li');
+  li.textContent = item;
+  ul.appendChild(li);
+});
+
