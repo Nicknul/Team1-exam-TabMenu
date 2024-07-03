3b7993e29f70bf75ee6d97edab576854290a5073
Nicknul <jea7730@gmail.com>
2024-07-03 16:06:04 +0900

    feat_정호연 : HTML 모둘화

---
+++ src/module/string_hy.ts
@@ -0,0 +1,22 @@
+const string = {
+  main: (content: string) => {
+    return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>MAIN</title>
+  </head>
+  <body>
+    <div id="root">
+      <h1>${content}</h1>
+    </div>
+  </body>
+</html>
+`;
+  },
+};
+
+console.log(string.main('정호연'));
+
+module.exports = string;
