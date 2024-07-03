877f7a7f6699c74878ec15762bea238840d515d3
KimInteger <kjs20151240@gmail.com>
2024-07-03 16:29:33 +0900

    feat_김정수 : TabMenu스켈레톤 완성

--- src/tabMenu_JS.ts
+++ src/tabMenu_JS.ts
@@ -2,11 +2,14 @@ const root : HTMLElement = document.getElementById('root') as HTMLDivElement;
 
 const ul : HTMLElement = document.createElement('ul') as HTMLUListElement;
 
+const content : HTMLDivElement = document.createElement('div');
+
 const nameArr : string[] = ["구하림","김정수","송이현","이연승","정호연"];
 
 nameArr.forEach((item : string, index : number) : void =>{
   const li : HTMLLIElement = document.createElement('li');
   li.innerHTML = `<a href="#menu${index}">${item}</a>`;
+  li.setAttribute('id', `menu${index}`)
   li.addEventListener('click',() : void => {
     console.log(window.location.hash);
   })
@@ -15,10 +18,25 @@ nameArr.forEach((item : string, index : number) : void =>{
 
 
 root.appendChild(ul);
+root.appendChild(content);
 
-// function changeTab() : void {
-//   const content : HTMLDivElement = document.createElement('div');
+function changeTab() : void {
+  content.textContent = '';
+  const startTarget = window.location.hash || "#menu0";
+  console.log(startTarget);
+  console.log(startTarget.substring(1));
+  const chooseTarget = document.getElementById(`${startTarget.substring(1)}`);
+  if(chooseTarget) {
+    if(chooseTarget?.textContent === '김정수') {
+      content.innerHTML = `<h1>안녕 난 ${chooseTarget.textContent}야</h1>`; 
+    } else {
+      content.innerHTML = `<h1>안녕 난 ${chooseTarget.textContent}이야</h1>`; 
+    }
+  } 
+};
 
-//   const startTarget = window.location.hash || "#menu0";
-//   const chooseTarget = document.
-// };
+document.addEventListener('DOMContentLoaded', function () {
+  changeTab();
+
+  window.addEventListener('hashchange', changeTab);
+});
