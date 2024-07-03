36f6dd0161455bb2c904c5bb172cdaaab2de45e4
KimInteger <kjs20151240@gmail.com>
2024-07-03 15:57:24 +0900

    feat_김정수 : window.location.hash값추적

--- src/tabMenu_JS.ts
+++ src/tabMenu_JS.ts
@@ -4,11 +4,21 @@ const ul : HTMLElement = document.createElement('ul') as HTMLUListElement;
 
 const nameArr : string[] = ["구하림","김정수","송이현","이연승","정호연"];
 
-nameArr.forEach((item : string) : void =>{
+nameArr.forEach((item : string, index : number) : void =>{
   const li : HTMLLIElement = document.createElement('li');
-  li.textContent = item;
+  li.innerHTML = `<a id="#menu${index}">${item}</a>`;
+  li.addEventListener('click',() : void => {
+    console.log(window.location.hash);
+  })
   ul.appendChild(li);
 });
 
+
 root.appendChild(ul);
 
+// function changeTab() : void {
+//   const content : HTMLDivElement = document.createElement('div');
+
+//   const startTarget = window.location.hash || "#menu0";
+//   const chooseTarget = document.
+// };
